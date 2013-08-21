8d4bcb2e321d7a081260ddbb5bc7e6ef35f09ec6
François Hodierne <francois@hodierne.net>
2013-08-21 09:15:57 +0200

    missing changes to table

--- model/table.class.php
+++ model/table.class.php
@@ -1,63 +1,114 @@
-<?php
-
-namespace Amateur\Model;
+<?php namespace Amateur\Model;
 
 class Table
 {
 
-  static $primary;
+  public $primary;
 
-  static $classname;
+  public $classname;
 
-  static $tablename;
+  public $tablename;
 
-  static $unique_indexes = [];
+  public $unique_indexes = [];
 
-  static function get($id)
+  function get($id)
   {
-    if (static::$primary) {
-      return static::get_one(static::$primary, $id);
+    if ($this->primary) {
+      return $this->get_one($this->primary, $id);
     }
   }
 
-  static function with_ids($ids)
+  function with_ids($ids)
   {
     $ressources = [];
-    foreach ($ids as $id) if ($ressource = static::get($id)) $ressources[] = $ressource;
+    foreach ($ids as $id) if ($ressource = $this->get($id)) $ressources[] = $ressource;
     return $ressources;
   }
 
-  static function get_one($key, $value)
+  function get_one($key, $value)
   {
-    // From Cache
-    $use_cache = in_array($key, static::$unique_indexes);
-    $cache_key = static::$tablename . "_one_" . $key . "_" . $value;
-    if ($use_cache && $ressource = cache_get($cache_key)) {
-      return $ressource;
-    }
-    // From DB
-    if ($row = db_get_one(static::$tablename, [$key => $value])) {
-      $ressource = new static::$classname($row);
-      // Set Cache
-      if ($use_cache) {
-        cache_set($cache_key, $ressource);
+    # From Cache
+    $use_cache = in_array($key, $this->unique_indexes);
+    $cache_key = $this->tablename . "_raw_" . $key . "_" . $value;
+    if ($use_cache) {
+      $row = cache_get($cache_key);
+      if (is_array($row)) {
+        return new $this->classname($row);
       }
-      return $ressource;
+      elseif ($row === 0) {
+        return null;
+      }
+    }
+    # From DB
+    $row = db_get_one($this->tablename, [$key => $value]);
+    if ($use_cache) {
+      cache_set($cache_key, $row ? $row : 0);
+    }
+    if ($row) {
+      return new $this->classname($row);
     }
   }
 
-  static function create($values = [])
+  function find_one($where = [])
   {
-    $result = db_insert(static::$tablename, $values);
-    if (static::$primary && $id = db_insert_id()) {
-      return static::get($id);
+    $row = db_get_one($this->tablename, $where);
+    if ($row) {
+      return new $this->classname($row);
     }
-    return new static::$classname($values);
   }
 
-  static function delete($where = [])
+  function search($params = [])
   {
-    return db_delete(static::$tablename, $where);
+    $result = db_search($this->tablename, $params);
+    return $result;
+  }
+
+  function create($values = [])
+  {
+    $result = db_insert($this->tablename, $values);
+    if ($this->primary && $id = db_insert_id()) {
+      # Update Cache
+      $row = db_get_one($this->tablename, [$this->primary => $id]);
+      foreach ($this->unique_indexes as $key) {
+        $cache_key = $this->tablename . "_raw_" . $key . "_" . $row[$key];
+        cache_set($cache_key, $row);
+      }
+      return new $this->classname($row);
+    }
+    return new $this->classname($values);
+  }
+
+  function update($ressource, $set = [])
+  {
+    if ($primary = $this->primary) {
+      $where = [$primary => $ressource->$primary];
+      # Update Db
+      $result = db_update($this->tablename, $set, $where);
+      # Update Cache
+      $row = db_get_one($this->tablename, $where);
+      foreach ($this->unique_indexes as $key) {
+        $cache_key = $this->tablename . "_raw_" . $key . "_" . $row[$key];
+        cache_set($cache_key, $row);
+      }
+      # Return new ressource
+      return new $this->classname($row);
+    }
+  }
+
+  function delete($ressource)
+  {
+    if ($primary = $this->primary) {
+      $where = [$primary => $ressource->$primary];
+      db_delete($this->tablename, $where);
+      foreach ($this->unique_indexes as $key) {
+        cache_delete($this->tablename . "_raw_" . $key . "_" . $ressource->$key);
+      }
+    }
+  }
+
+  function delete_where($where = [])
+  {
+    return db_delete($this->tablename, $where);
   }
 
 }
