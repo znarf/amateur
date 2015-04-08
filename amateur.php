4ab4510107edaaf3dfc566bb52241f209261ba6c
François Hodierne <francois@hodierne.net>
2015-04-08 22:26:11 +0200

    always load amateur.php

--- amateur.php
+++ amateur.php
@@ -2,7 +2,7 @@
 
 defined('amateur_dir') || define('amateur_dir', __dir__);
 
-# Autoload
+# Autoload (only if not already available)
 if (!class_exists('\amateur\core\loader')) {
   require_once amateur_dir . '/classes/core/loader.php';
   \amateur\core\loader::register_namespace('amateur', amateur_dir . '/classes');
