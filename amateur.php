0d50c76751c6e4e209e5c8fca6f3ef72468e32b8
François Hodierne <francois@hodierne.net>
2015-04-17 15:18:12 +0200

    remove class loader

--- amateur.php
+++ amateur.php
@@ -2,11 +2,5 @@
 
 defined('amateur_dir') || define('amateur_dir', __dir__);
 
-# Autoload Classes (if not already available)
-if (!class_exists('\amateur\amateur')) {
-  require_once amateur_dir . '/classes/loader.php';
-  \amateur\loader::register_namespace('amateur', amateur_dir . '/classes');
-}
-
 # Load Replaceables
 \amateur\replaceable::load(amateur_dir . '/replaceables', 'amateur');
