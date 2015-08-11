768a29cfb009465194cb1bfc169aed2a2d3a16f3
François Hodierne <francois@hodierne.net>
2015-08-12 01:58:06 +0200

    view -> helper

--- replaceables/default/default_helper.php
+++ replaceables/default/default_helper.php
@@ -5,5 +5,5 @@ function default_helper($name)
   if ($filename = amateur::filename('helper', $name)) {
     return include $filename;
   }
-  throw new exception("Unknown view ($name).", 500);
+  throw new exception("Unknown helper ($name).", 500);
 }
